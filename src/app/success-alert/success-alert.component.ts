fcb9825d080cb7a49dc796da875b446f26560543
Ivan Milanov <ivan.milanov@netcetera.com>
2019-07-28 14:37:45 +0200

    Implement assignment as required

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styles: [`.info-box-green {
+      color: navy;
+  }`]
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
