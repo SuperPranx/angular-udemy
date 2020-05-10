34249b5977e6bb392531ff67c249907410fc317f
Ivan Milanov <ivan85mk@yahoo.com>
2020-05-11 00:21:46 +0200

    - Use event handlers to fill the recipe details for a selected recipe

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,14 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {Recipe} from './recipe.model';
 
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.component.html',
   styleUrls: ['./recipe-book.component.css']
 })
-export class RecipeBookComponent implements OnInit {
+export class RecipeBookComponent {
+  selectedRecipe: Recipe;
 
-  constructor() { }
-
-  ngOnInit() {
+  onRecipeSelected(recipe: Recipe) {
+    this.selectedRecipe = recipe;
   }
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -6,15 +6,15 @@ import {Recipe} from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe 01', 'This is just a test 01', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
     new Recipe('A Test Recipe 02', 'This is just a test 02', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 
-  constructor() { }
-
-  ngOnInit() {
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
-
 }
