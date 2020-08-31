8d956caf9c9403e61c852ac97bb708b45fc52246
Ivan Milanov <ivan.milanov@netcetera.com>
2020-08-31 16:47:23 +0200

    - Add a host listener to the directive, so that it can react to parent events

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -9,7 +9,13 @@ export class BetterHighlightDirective implements OnInit {
               private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
   }
 
+  @HostListener('mouseenter') mouseenter(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  }
 }
