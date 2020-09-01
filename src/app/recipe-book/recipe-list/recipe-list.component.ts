60f03b88a46d1c3c0cd8b5b6acf46b86f8682125
Ivan Milanov <ivan.milanov@netcetera.com>
2020-09-01 13:36:09 +0200

    - Add Recipe and ShoppingList services and move recipe list to service

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,18 +1,21 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe 01', 'This is just a test 01', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A Test Recipe 02', 'This is just a test 02', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
-  ];
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeSelected.emit(recipe);
---
+++ src/app/recipe-book/recipe.service.ts
@@ -0,0 +1,15 @@
+import {Recipe} from './recipe.model';
+
+export class RecipeService {
+  constructor() {
+  }
+
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe 01', 'This is just a test 01', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A Test Recipe 02', 'This is just a test 02', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+  ];
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
+}
