73920589d4ca00a3e8c27e75016aeacff4718e96
Ivan Milanov <ivan.milanov@netcetera.com>
2020-09-01 17:07:13 +0200

    - Add ingredients to recipes

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,8 +9,24 @@ export class RecipeService {
   }
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe 01', 'This is just a test 01', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A Test Recipe 02', 'This is just a test 02', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe(
+      'A Test Recipe 01 (Pizza)',
+      'A tasty test pizza',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Pizza Dough', 1),
+        new Ingredient('Ketchup', 75),
+        new Ingredient('Cheese', 200),
+        new Ingredient('Pepperoni', 150)
+      ]),
+    new Recipe(
+      'A Test Recipe 02 (Chicken Soup)',
+      'A tasty test soup',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Chicken', 1),
+        new Ingredient('Soup', 1)
+      ])
   ];
 
   getRecipes(): Recipe[] {
