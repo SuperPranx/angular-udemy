2aae37d817da5ea54b174f8a96edffabf0c3092c
Ivan Milanov <ivan.milanov@netcetera.com>
2020-09-03 21:48:38 +0200

    - Changed images

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -14,7 +14,7 @@ export class RecipeService {
       1,
       'A Test Recipe 01 (Pizza)',
       'A tasty test pizza',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2014/07/08/12/34/pizza-386717_960_720.jpg',
       [
         new Ingredient('Pizza Dough', 1),
         new Ingredient('Ketchup', 75),
@@ -25,7 +25,7 @@ export class RecipeService {
       2,
       'A Test Recipe 02 (Chicken Soup)',
       'A tasty test soup',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2016/08/27/00/20/clear-broth-1623462_960_720.jpg',
       [
         new Ingredient('Chicken', 1),
         new Ingredient('Soup', 1)
