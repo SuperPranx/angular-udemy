fb7a08a14bb7454d51c1888f502eb56eb756b53d
Ivan Milanov <ivan.milanov@netcetera.com>
2020-09-06 17:14:53 +0200

    - Make validation message displa conditional, only when invalid and only if control has been touched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, ViewChild} from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {NgForm, NgModel} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +8,8 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('formElement') formElement: NgForm;
+  @ViewChild('email') email: NgModel;
   username: string;
-  email: string;
   secret: string;
 
   suggestUserName() {
