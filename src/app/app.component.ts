8fd57c4d5f102b9f6f7664084c6ebe6ef058f9cd
Ivan Milanov <ivan.milanov@netcetera.com>
2020-09-07 20:30:14 +0200

    - Add asynchronous validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
         username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        email: new FormControl(null, [Validators.required, Validators.email])
+        email: new FormControl(null, [Validators.required, Validators.email], this.fobiddenEmails)
       }),
       gender: new FormControl('male'),
       hobbies: this.hobbiesArray
@@ -36,5 +37,17 @@ export class AppComponent implements OnInit {
       return {nameIsForbidden: true};
     }
     return null;
+  }
+
+  fobiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({emailIsForbidden: true});
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
   }
 }
