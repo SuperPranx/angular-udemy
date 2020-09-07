3353ab564631fefd7c18157c03e03b991df9fa63
Ivan Milanov <ivan.milanov@netcetera.com>
2020-09-07 12:43:56 +0200

    - Create basic TD form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  readonly subscriptionTypes = ['Basic', 'Advanced', 'Pro'];
+  formData: {
+    email: '',
+    subType: '',
+    password: ''
+  };
 
+  handleSubmission(form: NgForm) {
+    this.formData = form.value.formData;
+    console.log(this.formData);
+    form.reset();
+  }
 }
